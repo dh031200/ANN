73a8df7f3312d156ea177fd91f73517a83eae379
dh031200 <dh031200@gmail.com>
2021-04-28 00:02:46 +0900

    Practice

--- Practice/Visualization_of_class_activation.py
+++ Practice/Visualization_of_class_activation.py
@@ -6,8 +6,6 @@ import numpy as np
 import cv2
 
 # Image preprocessing
-from Practice.Basic_of_matplotlib import draw_activation
-
 img_path = '../Binary_classification/cats_and_dogs/datasets/cats_and_dogs_small/test/cats/cat.1700.jpg'
 img = image.load_img(img_path, target_size=(224, 224))
 img_tensor = image.img_to_array(img)
@@ -33,6 +31,36 @@ def gradCAM(model, x):
     heatmap = np.maximum(heatmap, 0)
     heatmap /= np.max(heatmap)
     return heatmap, conv_layer_output_value, pooled_grads_value
+
+
+def deprocess_image(X):
+    X -= X.mean()
+    X /= (X.std() + 1e-5)
+    X *= 0.1
+    X += 0.5
+    X = np.clip(X, 0, 1)
+    X *= 255
+    X = np.clip(X, 0, 255).astype('uint8')
+    return X
+
+
+def draw_activation(activation, figure_name):
+    images_per_row = 16
+    n_features = activation.shape[-1]
+    size = activation.shape[1]
+    n_cols = n_features // images_per_row
+    display_grid = np.zeros((size * n_cols, images_per_row * size))
+    for col in range(n_cols):
+        for row in range(images_per_row):
+            channel_image = activation[0, :, :, col * images_per_row + row]
+            channel_image = deprocess_image(channel_image)
+            display_grid[col * size:(col + 1) * size, row * size:(row + 1) * size] = channel_image
+    scale = 1. / size
+    plt.figure(figsize=(scale * display_grid.shape[1], scale * display_grid.shape[0]))
+    plt.title(figure_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+    plt.show()
 
 
 # prediction
